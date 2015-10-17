31e127e65e05dc825e7fe1985efede0a29eb4cd5
Noitidart <noitidart@gmail.com>
2015-10-16 22:31:47 -0700

    1.0-night.rev2
    Blocking ALL requests which is good, now need to work to allow user
    interaction stuff to go through

--- resources/scripts/framescript.js
+++ resources/scripts/framescript.js
@@ -13,8 +13,76 @@ var rezStop = webNavigation.stop(Ci.nsIWebNavigation.STOP_ALL);
 console.error('should have stopped, rezStop:', rezStop);
 
 
-// lets now prevent future server requests
-// var webProgress = docShell.QueryInterface(Ci.nsIInterfaceRequestor).getInterface(Ci.nsIWebProgress);
-// webProgress.addProgressListener(this, Ci.nsIWebProgress.NOTIFY_STATE_WINDOW | Ci.nsIWebProgress.NOTIFY_LOCATION);
+// vars now prevent future server requests
+var myWebProgressListener = {
+	init: function() {
+		var webProgress = docShell.QueryInterface(Ci.nsIInterfaceRequestor).getInterface(Ci.nsIWebProgress);
+		webProgress.addProgressListener(this, Ci.nsIWebProgress.NOTIFY_STATE_WINDOW | Ci.nsIWebProgress.NOTIFY_LOCATION);
+	},
+	uninit: function() {
+		if (!docShell) {
+			return;
+		}
+		var webProgress = docShell.QueryInterface(Ci.nsIInterfaceRequestor).getInterface(Ci.nsIWebProgress);
+		webProgress.removeProgressListener(this);
+	},
 
+	onStateChange: function(webProgress, aRequest, stateFlags, status) {
+		console.log('onStateChange:', arguments);
+		if (aRequest) {
+			aRequest.cancel(Cr.NS_BINDING_ABORTED)
+		}
+		// var data = {
+		// 	requestURL: request.QueryInterface(Ci.nsIChannel).URI.spec,
+		// 	windowId: webProgress.DOMWindowID,
+		// 	parentWindowId: getParentWindowId(webProgress.DOMWindow),
+		// 	status,
+		// 	stateFlags,
+		// };
+
+		// if (webProgress.DOMWindow.top != webProgress.DOMWindow) {
+		// 	// this is a frame element
+		// 	var webNav = webProgress.QueryInterface(Ci.nsIWebNavigation);
+		// 	if (!webNav.canGoBack) {
+		// 		// For some reason we don't fire onLocationChange for the
+		// 		// initial navigation of a sub-frame. So we need to simulate
+		// 		// it here.
+		// 	}
+		// }
+	},
+	onLocationChange: function(webProgress, aRequest, locationURI, flags) {
+		console.log('onLocationChange:', arguments);
+		if (aRequest) {
+			aRequest.cancel(Cr.NS_BINDING_ABORTED)
+		}
+		// var data = {
+		// 	location: locationURI ? locationURI.spec : '',
+		// 	windowId: webProgress.DOMWindowID,
+		// 	parentWindowId: getParentWindowId(webProgress.DOMWindow),
+		// 	flags,
+		// };
+	},
+	QueryInterface: function QueryInterface(aIID) {
+		if (aIID.equals(Ci.nsIWebProgressListener) || aIID.equals(Ci.nsISupportsWeakReference) || aIID.equals(Ci.nsISupports)) {
+			return this;
+		}
+
+		throw Components.results.NS_ERROR_NO_INTERFACE;
+	}
+};
+
+myWebProgressListener.init();
 // end - addon functionalities
+
+// start - common helper functions
+function getWindowId(aWindow)
+{
+	return aWindow.QueryInterface(Ci.nsIInterfaceRequestor)
+				  .getInterface(Ci.nsIDOMWindowUtils)
+				  .outerWindowID;
+}
+
+function getParentWindowId(aWindow) {
+	return getWindowId(aWindow.parent);
+}
+// end - common helper functions
